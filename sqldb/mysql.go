345f626b7051a95b900ab16f5548fd2373c896ac
Goncharov Maksim <07sima07@gmail.com>
2021-01-16 06:23:28 +0300

    add biling

--- sqldb/mysql.go
+++ sqldb/mysql.go
@@ -4,7 +4,7 @@ import "database/sql"
 import _ "github.com/go-sql-driver/mysql"
 
 func ConnectDB() *sql.DB {
-	db, err := sql.Open("mysql", "max:1@/scrap")
+	db, err := sql.Open("mysql", "max@/scrap")
 	if err != nil {
 		panic(err)
 	}
